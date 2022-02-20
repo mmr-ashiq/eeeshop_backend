2996d1132057adb5815f4f270c07fa0d798dd75f
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2022-02-21 01:56:55 +0600

    refactor product, review and user model

--- src/modules/models/product.model.js
+++ src/modules/models/product.model.js
@@ -81,6 +81,10 @@ const ProductSchema = new mongoose.Schema(
 			type: Number,
 			default: 0,
 		},
+		numOfReviews: {
+			type: Number,
+			default: 0,
+		},
 		user: {
 			type: mongoose.Types.ObjectId,
 			ref: 'User',
@@ -99,6 +103,10 @@ ProductSchema.virtual('reviews', {
 	localField: '_id',
 	foreignField: 'product',
 	justOne: false,
+});
+
+ProductSchema.pre('remove', async function (next) {
+	await this.model('Review').deleteMany({ product: this._id });
 });
 
 module.exports = mongoose.model('Product', ProductSchema);

2c7c0e7ff6d1a7bda8c301f46b51077a29b92ad5
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2022-08-17 12:33:38 +0600

    final pushed 0.0.1

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ require('express-async-errors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+
+const swaggerUi = require('swagger-ui-express'),
+	swaggerDocument = require('./swagger.json');
 
 const connectDB = require('./src/db/connect');
 
@@ -17,6 +25,24 @@ const orderRouter = require('./src/modules/routes/order.route');
 const notFoundMiddleware = require('./src/modules/core/middlewares/not-found.middleware');
 const errorHandlerMiddleware = require('./src/modules/core/middlewares/error-handler.middleware');
 
+app.use(
+	'/api-docs',
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerDocument)
+);
+
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: 'Too many requests from this IP, please try again after an hour!'
+}));
+
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(cors());
+
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
@@ -25,7 +51,7 @@ app.use(express.static('./public'));
 app.use(fileUpload());
 
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.send('<h1>Welcome to the mmr-shop API</h1>');
 });
 
 app.use('/api/v1/auth', authRouter);
